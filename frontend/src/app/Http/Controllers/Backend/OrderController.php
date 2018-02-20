0c8f8669a5b1d8b604d65d29d3388eabf4b4ef8f
Martin Sanchez <martinsanchez@MacBook-Pro-de-Martin.local>
2018-02-20 10:36:07 -0300

    orderController

--- frontend/src/app/Http/Controllers/Backend/OrderController.php
+++ frontend/src/app/Http/Controllers/Backend/OrderController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use EventoOriginal\Core\Services\OrderService;
+use Illuminate\Support\Collection;
+use Yajra\DataTables\Contracts\DataTable;
 
 class OrderController extends Controller
 {
@@ -22,5 +24,29 @@ class OrderController extends Controller
         }
 
         return view('backend.admin.orders.show')->withOrder($order);
+    }
+
+    public function index()
+    {
+        return view('backend.admin.orders.index');
+    }
+
+    public function getDataTables()
+    {
+        $orders = $this->orderService->findAll();
+
+        $ordersCollection = new Collection();
+
+        foreach ($orders as $order) {
+            $ordersCollection->push([
+                'Id' => $order->getId(),
+                'Fecha' => $order->getCreateDate()->format('Y-m-d'),
+                'Estado' => $order->getStatus(),
+                'Tipo' => '',
+                'Opciones' => ''
+            ]);
+        }
+
+        return DataTable::of($ordersCollection)->make(true);
     }
 }
